78dccab8a9f2e833b468e4433b5244a36cba0b31
Kevin Ellis <kevin.ellis@excella.com>
2021-04-14 15:26:56 -0700

    add d3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import VisualizationContainer from './VisualizationContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <VisualizationContainer />
     </div>
   );
 }
---
+++ src/VisualizationContainer.js
@@ -0,0 +1,11 @@
+function VisualizationContainer(props) {
+
+    return (
+        <div>
+            <h1>Visualization</h1>
+            
+        </div>
+    );
+}
+
+export default VisualizationContainer;
