8f6ee440d9aa6a380410b2204797ee47dbd480a8
Kevin Ellis <kevin.ellis@excella.com>
2021-04-14 17:08:38 -0700

    remove svg

--- src/VisualizationContainer.js
+++ src/VisualizationContainer.js
@@ -3,21 +3,9 @@ function VisualizationContainer(props) {
     return (
         <div>
             <h3>Visualization</h3>
-            <Svg />
+            
         </div>
     );
 }
-
-const Svg = () => {
-    return (
-        <svg>
-        <circle
-          cx="150"
-          cy="77"
-          r="40"
-        />
-      </svg>
-    )
-  }
 
 export default VisualizationContainer;
